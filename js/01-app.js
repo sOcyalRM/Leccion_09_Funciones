244a016c1b1a6334351d0c8f19da7b6db730fd1d
sOcyalRM <oscalyromat@icloud.com>
2026-03-03 21:59:44 -0400

    Inicio de leccion 9, Funcionesj. Agregando archivos descargados

---
+++ js/01-app.js
@@ -0,0 +1,15 @@
+//Function Declaration - Declaracion de funciones
+function sumar() {
+    console.log(2 + 2);
+}
+
+sumar();
+sumar(); //Son recursivas
+
+
+//Expresion de Funcion - Function Expression
+const resultado = function(){
+    console.log (3+3);
+}
+
+resultado();
